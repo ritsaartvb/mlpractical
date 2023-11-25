8ce3fb0d58dab668ff8246394d67dca59f6d53b7
Ritsaart van Blankenstein <ritsaart.b.b@gmail.com>
2023-11-25 13:24:09 +0000

    fix grad flow ii

--- pytorch_mlp_framework/experiment_builder.py
+++ pytorch_mlp_framework/experiment_builder.py
@@ -154,7 +154,7 @@ class ExperimentBuilder(nn.Module):
         Complete the code in the block below to collect absolute mean of the gradients for each layer in all_grads with the             layer names in layers.
         """
         ########################################
-        ffor n, p in named_parameters:
+        for n, p in named_parameters:
             if (p.requires_grad) and ("bias" not in n):
                 layers.append(n)
                 # Move gradients to CPU memory before calculating mean
